7f7a52a9c678905fcd92e076e8cbd09396de69df
angelluiss12 <angel.rangel@mykukun.com>
2022-09-22 18:27:59 -0500

    Initial Commit

---
+++ ceibatestApp/ceibatestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ceibatestAppApp.swift
+//  ceibatestApp
+//
+//  Created by inicial on 22/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ceibatestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
